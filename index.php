033ce3fc9939d556847f66793953a080cb578e46
graciasantini <96924146+graciasantini@users.noreply.github.com>
2022-10-20 08:51:32 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>The genius coorporation</title>
+</head>
+<body>
+    <?php 
+    include("pages/header.php");
+    ?>
+
+    <?php 
+    include("pages/main.php");
+    ?>
+
+    <?php 
+    include("pages/footer.php");
+    ?>
+    <script src="js/script.js">
+    </script>
+</body>
+</html>
